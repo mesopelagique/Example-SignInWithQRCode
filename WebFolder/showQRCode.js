a7d3805ea229aa88d159fc81df0662f53c8a00e5
eric.marchand <eric.marchand@4d.com>
2022-12-12 16:04:46 +0100

    Some border for QR code image
    code formatting

--- WebFolder/showQRCode.js
+++ WebFolder/showQRCode.js
@@ -1,41 +1,40 @@
-﻿
-function loadDoc(url, cFunction) {
-	var xhttp;
-	xhttp=new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-		cFunction(this);
-		}
-	};
-	xhttp.open("GET", url, true);
-	xhttp.send();
+﻿function loadDoc(url, cFunction) {
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cFunction(this);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
 }
 
 function showQRCodeCallback(response) {
-	var text = response.responseText;
-	document.getElementById("qrcode").innerHTML = "";
-	if (text) {
-		var qrcode = new QRCode(document.getElementById("qrcode"), {
-			text: text,
-			width: 128,
-			height: 128,
-			colorDark : "#03e9f4",
-			colorLight : "#ffffff",
-			correctLevel : QRCode.CorrectLevel.H
-		});
-	} else {
-		document.getElementById("qrcode").innerHTML = "Failed to generate qr code";
-	}
+    var text = response.responseText;
+    document.getElementById("qrcode").innerHTML = "";
+    if (text) {
+        var qrcode = new QRCode(document.getElementById("qrcode"), {
+            text: text,
+            width: 256,
+            height: 256,
+            colorDark: "#0c989f",
+            colorLight: "#ffffff",
+            correctLevel: QRCode.CorrectLevel.H
+        });
+    } else {
+        document.getElementById("qrcode").innerHTML = "Failed to generate qr code";
+    }
 }
 
 function showQRCode() {
-	loadDoc("4DAction/qrcodeData", showQRCodeCallback);
+    loadDoc("4DAction/qrcodeData", showQRCodeCallback);
 }
 
 function logoutCallback(response) {
-	window.location.href = "/";
+    window.location.href = "/";
 }
 
 function logout() {
-	loadDoc("4DAction/logout", logoutCallback);
+    loadDoc("4DAction/logout", logoutCallback);
 }
